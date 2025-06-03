67da5d331b5ba54c47ee88c4996d7adac0aaf408
Miguel Costa <miguel.costa@qt.io>
2025-06-03 16:20:44 +0200

    Add Qt.Exclude and Qt.Ignore attribs
    Task-number: QTBUG-134964
    Change-Id: Ic1e56d9abc8fe0132c9662898e65f90292f84d3d
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

---
+++ src/Qt.DotNet.Adapter/Qt/QtAttribs.cs
@@ -0,0 +1,37 @@
+/***************************************************************************************************
+ Copyright (C) 2025 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+using Qt.MetaObject;
+using System;
+
+namespace Qt
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class ExcludeAttribute : Attribute
+    {
+        public ExcludeAttribute(params Type[] excludedTypes)
+        { }
+        public ExcludeAttribute(params string[] excludedTypeNames)
+        { }
+        public bool Inherited { get; set; } = false;
+    }
+
+    [AttributeUsage(TypeAttributeTarget | MemberAttributeTarget, AllowMultiple = false)]
+    public class IgnoreAttribute : Attribute
+    {
+        private const AttributeTargets TypeAttributeTarget
+            = AttributeTargets.Class
+            | AttributeTargets.Struct
+            | AttributeTargets.Interface
+            | AttributeTargets.Enum
+            | AttributeTargets.Delegate;
+        private const AttributeTargets MemberAttributeTarget
+            = AttributeTargets.Constructor
+            | AttributeTargets.Method
+            | AttributeTargets.Property
+            | AttributeTargets.Field
+            | AttributeTargets.Event;
+    }
+}
