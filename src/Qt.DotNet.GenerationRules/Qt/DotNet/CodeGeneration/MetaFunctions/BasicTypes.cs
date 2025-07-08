0fbe249100c4bd1f7e3367dddf1ebeb81e0202ff
Miguel Costa <miguel.costa@qt.io>
2025-07-08 13:25:11 +0200

    Generate basic types
    Task-number: QTBUG-134967
    Change-Id: Ie448bec180999edc744894279f8102a0e54a334a
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

---
+++ src/Qt.DotNet.GenerationRules/Qt/DotNet/CodeGeneration/MetaFunctions/BasicTypes.cs
@@ -0,0 +1,44 @@
+/***************************************************************************************************
+ Copyright (C) 2025 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+using System.Reflection;
+
+namespace Qt.DotNet.CodeGeneration.MetaFunctions
+{
+    using Extensions;
+    using static Traits;
+
+    public class BasicTypes : MetaFunction
+    {
+        protected override string Eval(object src, Enum traits) => src switch
+        {
+            Type type when type.IsBuiltIn() => type switch
+            {
+                _ when traits.HasTraits(Src) => type.FullName,
+                _ when type.Is<sbyte>() => "qint8",
+                _ when type.Is<byte>() => "quint8",
+                _ when type.Is<short>() => "qint16",
+                _ when type.Is<ushort>() => "quint16",
+                _ when type.Is<int>() => "qint32",
+                _ when type.Is<uint>() => "quint32",
+                _ when type.Is<long>() => "qint64",
+                _ when type.Is<ulong>() => "quint64",
+                _ when type.Is<nint>() => "void *",
+                _ when type.Is<nuint>() => "void *",
+                _ when type.Is<float>() => "float",
+                _ when type.Is<double>() => "double",
+                _ when type.Is<bool>() => "bool",
+                _ when type.Is<char>() => "QChar",
+                _ when type.Is<decimal>() => "double",
+                _ when type.Is<string>() => "QString",
+                _ when type.Is<object>() => "QDotNetRef",
+                _ when type.Is<Type>() => "QDotNetType",
+                _ when type.Is(typeof(void)) => "void",
+                _ => null
+            },
+            _ => null
+        };
+    }
+}
