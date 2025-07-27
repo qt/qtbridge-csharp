dbabe130fc216c35a0e941d03d7e34af7b1973c7
Miguel Costa <miguel.costa@qt.io>
2025-07-27 20:26:02 +0200

    Add code generation "dummy" project
    Task-number: QTBUG-134965
    Task-number: QTBUG-134983
    Change-Id: I9fd26b418d3e988c903ec230dea0caecf268f816
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

---
+++ tests/GeneratorTestApp/Prime.cs
@@ -0,0 +1,65 @@
+﻿/***************************************************************************************************
+ Copyright (C) 2025 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+using System.ComponentModel;
+using Qt.DotNet.Utils;
+
+namespace GeneratorTestApp
+{
+    public class Prime : INotifyPropertyChanged
+    {
+        public int Index
+        {
+            get => lazy.Get(() => Index, () => 0);
+            set
+            {
+                lazy.Set(() => Index, value);
+                Value = NthPrime(value + 1);
+            }
+        }
+
+        public int this[int idx] { get => NthPrime(idx); set { } }
+        public int this[string idx] { get => NthPrime(int.Parse(idx)); set { } }
+
+        public int Value
+        {
+            get => lazy.Get(() => Value, () => 2);
+            set => lazy.Set(() => Value, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { lazy.PropertyChanged += value; }
+            remove { lazy.PropertyChanged -= value; }
+        }
+
+        private LazyFactory lazy = new();
+
+        private static int NthPrime(int n)
+        {
+            int x = 2;
+            while (n > 0) {
+                if (IsPrime(x))
+                    --n;
+                ++x;
+            }
+            return x - 1;
+        }
+
+        private static bool IsPrime(int x)
+        {
+            if (x <= 1)
+                return false;
+            if (x == 2 || x == 3)
+                return true;
+            if (x % 2 == 0 || x % 3 == 0)
+                return false;
+            for (int i = 5; i * i <= x; i += 6)
+                if (x % i == 0 || x % (i + 2) == 0)
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ tests/GeneratorTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿/***************************************************************************************************
+ Copyright (C) 2025 The Qt Company Ltd.
+ SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+***************************************************************************************************/
+
+namespace GeneratorTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Escape)
+                if (!Console.KeyAvailable)
+                    Thread.Sleep(100);
+        }
+    }
+}
