1c0408fe49c847dc3bc74f791bb24fa97a769323
Karsten Heimrich <karsten.heimrich@qt.io>
2026-03-11 13:01:02 +0100

    Limit generated object path length on Windows explicitly
    Change-Id: Ib21768c693b86baef701bee51a5566c125938ba9
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

--- src/Qt.Bridge.CSharp.GenerationRules/Qt/DotNet/CodeGeneration/Rules/GenerateBuildSpec.cs
+++ src/Qt.Bridge.CSharp.GenerationRules/Qt/DotNet/CodeGeneration/Rules/GenerateBuildSpec.cs
@@ -34,6 +34,10 @@ cmake_minimum_required(VERSION 3.16)
 
 project({Root.MFn(Target)} VERSION {Root.MFn(Version)} LANGUAGES CXX)
 
+if (WIN32)
+    set(CMAKE_OBJECT_PATH_MAX 128)
+endif()
+
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
