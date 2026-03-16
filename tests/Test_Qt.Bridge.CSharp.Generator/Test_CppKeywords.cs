9d58a7ba0d1e5966fec25168a029aa1926d62fc2
Karsten Heimrich <karsten.heimrich@qt.io>
2026-03-16 14:48:58 +0100

    Stabilize CppKeywords formatting test
    Ignore formatting-only generator output differences.
    
    Change-Id: I0916bfa66c71894a1999955e551f4a2c2900cc6c
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

--- tests/Test_Qt.Bridge.CSharp.Generator/Test_CppKeywords.cs
+++ tests/Test_Qt.Bridge.CSharp.Generator/Test_CppKeywords.cs
@@ -32,48 +32,23 @@ namespace Test_Qt.Bridge.CSharp.Generator
         public async Task CppKeywords()
         {
             using var result = await TestCodeGenerator.GenerateAsync([Source]);
-            Assert.Contains(@"
-class template_::noexcept_ :
-", result.CombinedText);
-            Assert.Contains(@"
-    EVENT_Protected Q_SIGNAL void protected_(QObject *qEvArgs);
-", result.CombinedText);
-            Assert.Contains(@"
-    Q_PROPERTY(qint32 default_ READ default_ WRITE setDefault)
-", result.CombinedText);
-            Assert.Contains(@"
-    Q_PROPERTY(System::Collections::Generic::List_1__noexcept__typedef_ *friend_ READ friend_ WRITE setFriend)
-", result.CombinedText);
-            Assert.Contains(@"
-    Q_INVOKABLE void break_(bool constexpr_) const;
-", result.CombinedText);
-            Assert.Contains(@"
-template_::noexcept_::noexcept_() :
-    QDotNetObject(constructor<template_::noexcept_>().invoke(nullptr)),
-    d(new template_::noexcept_Private(this)),
-    i(new template_::noexcept_Init(this, d))
-", result.CombinedText);
-            Assert.Contains(@"
-template_::noexcept_::~noexcept_()
-", result.CombinedText);
-            Assert.Contains(@"
-void template_::noexcept_::break_(bool constexpr_) const
-", result.CombinedText);
-            Assert.Contains(@"
-class template_::noexcept__typedef_Enum : public QObject
-{
-    Q_OBJECT
-    QML_NAMED_ELEMENT(noexcept__typedef_)
-    QML_UNCREATABLE(""Type 'noexcept__typedef_' is an enum."")
-public:
-    enum Values
-    {
-        z_asm = 0,
-        auto_ = 1
-    };
-    Q_ENUM(Values)
-};
-", result.CombinedText);
+            Assert.Contains("class template_::noexcept_ :", result.CombinedText);
+            Assert.Contains("EVENT_Protected Q_SIGNAL void protected_(QObject *qEvArgs);", result.CombinedText);
+            Assert.Contains("Q_PROPERTY(qint32 default_ READ default_ WRITE setDefault)", result.CombinedText);
+            Assert.Contains("Q_PROPERTY(System::Collections::Generic::List_1__noexcept__typedef_ *friend_ READ friend_ WRITE setFriend)", result.CombinedText);
+            Assert.Contains("Q_INVOKABLE void break_(bool constexpr_) const;", result.CombinedText);
+            Assert.Contains("template_::noexcept_::noexcept_() :", result.CombinedText);
+            Assert.Contains("QDotNetObject(constructor<template_::noexcept_>().invoke(nullptr))", result.CombinedText);
+            Assert.Contains("d(new template_::noexcept_Private(this))", result.CombinedText);
+            Assert.Contains("i(new template_::noexcept_Init(this, d))", result.CombinedText);
+            Assert.Contains("template_::noexcept_::~noexcept_()", result.CombinedText);
+            Assert.Contains("void template_::noexcept_::break_(bool constexpr_) const", result.CombinedText);
+            Assert.Contains("class template_::noexcept__typedef_Enum : public QObject", result.CombinedText);
+            Assert.Contains("QML_NAMED_ELEMENT(noexcept__typedef_)", result.CombinedText);
+            Assert.Contains("QML_UNCREATABLE(\"Type 'noexcept__typedef_' is an enum.\")", result.CombinedText);
+            Assert.Contains("z_asm = 0", result.CombinedText);
+            Assert.Contains("auto_ = 1", result.CombinedText);
+            Assert.Contains("Q_ENUM(Values)", result.CombinedText);
         }
     }
 }
