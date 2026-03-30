853314bf3b2031196c4ed25825f823a10aa884c4
Karsten Heimrich <karsten.heimrich@qt.io>
2026-03-30 12:36:44 +0200

    Add XML documentation for QmlNaming static API
    Change-Id: I8ac7eaee632c0b55a2058f58fbf4d734611fac2c
    Reviewed-by: Miguel Costa <miguel.costa@qt.io>

--- src/Qt.Bridge.CSharp.Api/Qt/Quick/QmlNaming.cs
+++ src/Qt.Bridge.CSharp.Api/Qt/Quick/QmlNaming.cs
@@ -5,14 +5,40 @@ using Qt.Bridge.Utils.Text;
 
 namespace Qt.Quick
 {
+    /// <summary>
+    /// Normalizes .NET and QML identifiers to the casing conventions used by QML.
+    /// </summary>
     public static class QmlNaming
     {
+        /// <summary>
+        /// Converts a .NET-style property name such as <c>"DisplayName"</c> to the lower-camel
+        /// form expected by QML property and role names, such as <c>"displayName"</c>.
+        /// </summary>
+        /// <remarks>
+        /// QML requires property names to begin with a lower-case letter and contain only letters,
+        /// numbers and underscores. See
+        /// <see href="https://doc.qt.io/qt-6/qtqml-syntax-objectattributes.html#:~:text=Property%20names%20must%20begin%20with,of%20the%20property%20being%20declared.">
+        /// QML Object Attributes</see>.
+        /// </remarks>
         public static string ToQmlPropertyName(this string text)
             => text.ConvertCase(CaseStyle.Pascal, CaseStyle.Camel);
 
+        /// <summary>
+        /// Converts a QML-style property or role name such as <c>"displayName"</c> to the Pascal
+        /// case typically used by .NET property names, such as <c>"DisplayName"</c>.
+        /// </summary>
         public static string ToDotNetPropertyName(this string text)
             => text.NormalizeQmlTypeName();
 
+        /// <summary>
+        /// Normalizes a QML type name to the PascalCase form required for QML object types.
+        /// For example, <c>"mainWindow"</c> becomes <c>"MainWindow"</c>.
+        /// </summary>
+        /// <remarks>
+        /// QML requires type names to begin with an upper-case letter. See
+        /// <see href="https://doc.qt.io/qt-6/qtqml-documents-definetypes.html#naming-custom-qml-object-types">
+        /// Naming Custom QML Object Types</see>.
+        /// </remarks>
         public static string NormalizeQmlTypeName(this string text)
             => text.ConvertCase(CaseStyle.Camel, CaseStyle.Pascal);
     }
