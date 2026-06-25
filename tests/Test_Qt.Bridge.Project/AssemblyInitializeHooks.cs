917e3040a756126dc6b67fa3399d6fedf7e0f50b
Miguel Costa <miguel.costa@qt.io>
2026-06-25 14:05:46 +0200

    tests: Work around vstools setting QtDir
    The vstools extension sets the QtDir env.var. for the IDE process, which
    is then carried over to processes created while running auto-tests. This
    clashes with the use of QtDir in Qt.Bridge.targets, causing tests to
    fail. To work around this, when loading the test assembly, we now check
    if QtDir is set for the process, and reset it to the registry value that
    applies to the current user or to the local machine.
    
    This workaround is only relevant when starting tests from the VS IDE. As
    such, resetting QtDir only happens if the OS is Windows. For other
    platforms, if QtDir is set, a message is printed but the value of the
    env. var. will not be changed.
    
    Change-Id: Iaed00d745939d16a9fd44d643203b68552c28fe9
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- tests/Test_Qt.Bridge.Project/AssemblyInitializeHooks.cs
+++ tests/Test_Qt.Bridge.Project/AssemblyInitializeHooks.cs
@@ -1,6 +1,7 @@
 // Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test_Qt.Bridge.Project
@@ -11,6 +12,23 @@ namespace Test_Qt.Bridge.Project
         [AssemblyInitialize]
         public static void Initialize(TestContext context)
         {
+            var qtDir = Environment
+                .GetEnvironmentVariable("QtDir", EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(qtDir)) {
+                Console.WriteLine($"QTDIR is set: {qtDir}");
+                if (OperatingSystem.IsWindows()) {
+                    var sysQtDir = Environment.GetEnvironmentVariable(
+                            "QtDir", EnvironmentVariableTarget.User)
+                        ?? Environment.GetEnvironmentVariable(
+                            "QtDir", EnvironmentVariableTarget.Machine);
+                    if (qtDir != sysQtDir) {
+                        Console.WriteLine($"Resetting QTDIR to: {sysQtDir}");
+                        Environment.SetEnvironmentVariable(
+                            "QtDir", sysQtDir, EnvironmentVariableTarget.Process);
+                    }
+                }
+            }
+
             TempProject.CleanupProjectRoot();
         }
     }
